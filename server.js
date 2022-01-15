73737ea4fc68938abb45cf7f156595f4670475a0
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-15 16:09:31 -0500

    add boilerplate sections to server.js

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const express = require("express");
 
 // Initialize application
 const app = express();
+
+// Configure application settings
+
+// Connect to database
+
+// Mount middleware
+
+// Mount routes
+
+// Tell app to listen on PORT
