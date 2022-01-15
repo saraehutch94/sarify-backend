c6a826e32d6daa1ee987d6a622cbea96ab83e959
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-15 16:43:57 -0500

    add index/test route w router object

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -4,3 +4,10 @@ const Track = require("../models/track");
 
 // Set up router object
 const trackRouter = express.Router();
+
+// Set up routes
+
+// index/test route
+trackRouter.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
