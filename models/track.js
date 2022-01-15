069c5f25cf1f26946eece9b2fb4f785801405f51
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-15 16:37:34 -0500

    create track schema

--- models/track.js
+++ models/track.js
@@ -3,3 +3,15 @@ const mongoose = require("mongoose");
 
 // Schema variable
 const Schema = mongoose.Schema;
+
+// Track Schema
+const trackSchema = new Schema(
+  {
+    title: String,
+    artist: String,
+    coverArt: String,
+    description: String,
+    genre: String,
+  },
+  { timestamps: true }
+);
