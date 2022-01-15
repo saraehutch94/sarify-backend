34e35d332c34757f436e0de3b0c4923877357619
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-01-15 16:51:43 -0500

    set up routes boilerplate in controller file

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -7,10 +7,20 @@ const trackRouter = express.Router();
 
 // Set up routes
 
-// index/test route
-trackRouter.get("/", (req, res) => {
-  res.send("Hello World!!!");
-});
+// // test route
+// trackRouter.get("/", (req, res) => {
+//   res.send("Hello World!!!");
+// });
+
+// index route
+
+// delete route
+
+// update route
+
+// create route
+
+// show route
 
 // Export router object
 module.exports = trackRouter;
